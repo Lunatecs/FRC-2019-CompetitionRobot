19c24b0531cc6eb9293684756ed29ddf614b577d
rissy901 <rissy1971@gmail.com>
2019-02-23 16:53:25 -0500

    Added most cargo set points for rocket.

--- src/main/java/frc/robot/commands/wrist/WristWithJoystick.java
+++ src/main/java/frc/robot/commands/wrist/WristWithJoystick.java
@@ -26,7 +26,7 @@ public class WristWithJoystick extends Command {
   @Override
   protected void execute() {
 
-    Robot.wrist.setWristSpeed(-Robot.oi.getWristSpeed());
+    Robot.wrist.setWristSpeed(Robot.oi.getWristSpeed());
   }
 
   // Make this return true when this Command no longer needs to run execute()
