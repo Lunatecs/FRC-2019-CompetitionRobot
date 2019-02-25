6738571a1e904a62854756de640bec3f739d1137
baxtrax <bradofficialbox@gmail.com>
2019-02-25 10:50:58 -0500

    Added HatchRocket and ResetElevatorAndWristPosition commands

---
+++ src/main/java/frc/robot/commands/autos/HatchRocket.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.autos;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import frc.robot.commands.autos.ResetElevatorAndWristPosition;
+import frc.robot.commands.elevator.ElevatorWithSetPoint;
+import frc.robot.commands.wrist.WristSetPosition;
+import frc.robot.commands.intake.LaunchHatch;
+
+public class HatchRocket extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public HatchRocket(double wristPos, int elevatorPos) {
+    addParallel(new WristSetPosition(wristPos));
+    //Will wait for elevatorwithsetpoint PID to finish, then will launch hatch 
+    addSequential(new ElevatorWithSetPoint(elevatorPos));
+    //addSequential(new WaitCommand(.1));
+    addSequential(new LaunchHatch());
+    //Wait for hatch to clear beak before reset
+    addSequential(new WaitCommand(.5));
+    //Raise and lower beak to avoid hitting the beak against rocket while resetting
+    addSequential(new ResetElevatorAndWristPosition());
+  }
+}
---
+++ src/main/java/frc/robot/commands/autos/ResetElevatorAndWristPosition.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.autos;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import frc.robot.commands.elevator.ElevatorWithSetPoint;
+import frc.robot.commands.wrist.WristSetPosition;
+import frc.robot.commands.intake.RaiseBeak;
+import frc.robot.commands.intake.LowerBeak;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Wrist;
+
+public class ResetElevatorAndWristPosition extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public ResetElevatorAndWristPosition() {
+    //TODO find position that we want both wrist and elevator to reset to
+    addSequential(new RaiseBeak());
+    //Wait till Beak is raised so it wont hit rocket on way down, may not need
+    addSequential(new WaitCommand(.05));
+    addParallel(new ElevatorWithSetPoint(Elevator.ROCKET_LOWER_HATCH));
+    addSequential(new WristSetPosition(Wrist.NINETY_DEGREE));
+    addSequential(new LowerBeak());
+  }
+}
